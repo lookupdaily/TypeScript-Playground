2f0aab71eda05698c6091f3fe2c3bd286798a322
Liz Daly <liz7daly@gmail.com>
2020-07-08 18:51:58 +0100

    Test that multiples of 3 are fizz

--- fizzbuzz/fizzbuzz.test.ts
+++ fizzbuzz/fizzbuzz.test.ts
@@ -20,4 +20,9 @@ describe('fizzbuzz', () => {
     expect(fizzBuzz.run(5)).toEqual(["1","2","fizz","4","buzz"])
   })
 
+  it('multiples of 3 are fizz', () => {
+    const fizzBuzzSix = fizzBuzz.run(6)[5]
+    expect(fizzBuzzSix).toEqual("fizz")
+  })
+
 });
